9681dd090e12d18a52d5d60808a8179f00558226
elias <defreitase@bzz.ch>
2024-01-09 13:19:13 +0100

    test-push

---
+++ app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask, render_template
+from flask_sqlalchemy import sqlalchemy
+print(sqlalchemy.__version__)
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
