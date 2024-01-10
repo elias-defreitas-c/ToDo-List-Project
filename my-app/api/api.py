a49c50f5277f32a2a13d1a2e171c574a17c8ee5d
elias <defreitase@bzz.ch>
2024-01-10 13:06:57 +0100

    push 4

--- my-app/api/api.py
+++ my-app/api/api.py
@@ -2,8 +2,7 @@ from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
 db = SQLAlchemy(app)
 
 
