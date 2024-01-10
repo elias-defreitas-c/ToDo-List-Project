e4ddfe072aae60e3fcd299385b3022dd52744806
elias <defreitase@bzz.ch>
2024-01-10 10:14:21 +0100

    test-push

--- app.py
+++ app.py
@@ -1,18 +1,31 @@
-
-from flask import Flask, render_template
-from flask_sqlalchemy import sqlalchemy
-print(sqlalchemy.__version__)
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-@app.route('/')
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+
+    def __str__(self):
+        return f'{self.id} {self.title}'
+
+def todo_serializer(todo):
+    return {
+        'id': todo.id,
+        'title': todo.title
+    }
+@app.route('/api', methods={'GET'})
 def index():
-    return render_template('base.html')
+    return jsonify([*map(todo_serializer, Todo.query.all())])
+
 
 
 if __name__ == "__main__":
+    app.app_context().push()
     db.create_all()
     app.run(debug=True)
