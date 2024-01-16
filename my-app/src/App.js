9efe0d8fc364b6feb171dceb3b34b752175c7c8c
elias <defreitase@bzz.ch>
2024-01-16 11:17:31 +0100

    little push

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 import { TodoPage } from "./Pages/TodoPage";
+import { Show } from "./Pages/Show";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <TodoPage/>
+        <Router>
+            <Routes>
+                <Route exact path={'/'} element={<TodoPage />} />
+                <Route path={'/:id'} element={<Show />} />
+            </Routes>
+        </Router>
     </div>
   );
 }
